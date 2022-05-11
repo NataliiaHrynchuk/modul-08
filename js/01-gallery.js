fcc397750eda3d632b4a71635a8444a5758ebdf5
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-05-11 14:48:28 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
-// 'control + ~'
+console.log('module 9.1');
+
+setTimeout(function timeout () {
+    console.log('module 9.2');
+}, 2000)
+
+
+const x = 2;
+const y = 3;
+
+function sum(a, b) {
+    console.log(a + b);
+
+}
+console.log('module 9.3');
+setTimeout(sum, 1000, x, y,);
+console.log('module 9.4');
+
+for (var i = 0; i < 3; i++) {
+    setTimeout(
+        function (y) {
+            console.log(y);
+        }, 1000, i);
+    
+}
+
+const date = new Date();
+
+console.dir(date);
+// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+console.log(date.getMonth());
+console.log(date.getDate());
+
+console.dir(date.toString());
+// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+console.log(moment);
+//====================== Приклад з Кахуту ==============
+for (let i = 3; i > 0; i--) {
+    const delite = i * 1000;
+    setTimeout(console.log(i), delite);
+}
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,15 @@
+// =====Репета Модуль 8 Бібліотеки====
+//--Перетворення об'єкту в рядок-----
+const user = {
+    name: 'Mango',
+    age: 2
+}
 
+console.log(JSON.stringify(user));//{"name":"Mango","age":2}
+
+// -----Перетворення рядка в джаваскрипт-об'єкт---
+const saveUserData = '{"name":"Mango", "age":2}';
+console.log(JSON.parse(saveUserData));//{name: 'Mango', age: 2}
+
+//--------LocalStorage--------------
+console.log(localStorage);
