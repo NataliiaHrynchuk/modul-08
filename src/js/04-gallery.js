65266fcb172fef6f5a3a10c679aa9e81c9b8a271
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-05-13 16:31:15 +0300

    update

---
+++ src/js/04-gallery.js
@@ -0,0 +1,8 @@
+import '../css/common.css';
+import'../css/01-gallery.css';
+import countries from './countries.json';
+import itemsTemplate from '../templates/gallery-items.hbs';
+
+const galleryRef = document.querySelector('.js-gallery');
+const markup = itemsTemplate(countries);
+galleryRef.insertAdjacentHTML('beforeend', markup);
